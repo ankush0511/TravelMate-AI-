ac3a23a7e101606b29e4026a2d938de7629bb222
ankush0511 <136951570+ankush0511@users.noreply.github.com>
2025-02-28 21:09:55 +0530

    added agentfile

---
+++ TravelAgents.py
@@ -0,0 +1,49 @@
+from crewai import Agent
+from TravelTools import search_web_tool
+#from TravelTools import search_web_tool, web_search_tool
+from crewai import LLM
+import os
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+import streamlit as st
+
+load_dotenv()
+# GROQ_API_KEY=st.secrets["GROQ_API_KEY"]
+groq_api_key = os.getenv("GROQ_API_KEY")
+llm = ChatGroq(api_key=groq_api_key, model='groq/Gemma2-9b-It')
+
+# Agents
+guide_expert = Agent(
+    role="City Local Guide Expert",
+    goal=
+    "Provides information on things to do in the city based on user interests.",
+    backstory="A local expert passionate about sharing city experiences.",
+    tools=[search_web_tool],
+    verbose=True,
+    max_iter=5,
+    llm=ChatGroq(api_key=groq_api_key, model='groq/Gemma2-9b-It'),
+    allow_delegation=False,
+)
+
+location_expert = Agent(
+    role="Travel Trip Expert",
+    goal="Provides travel logistics and essential information.",
+    backstory=
+    "A seasoned traveler who knows everything about different cities.",
+    tools=[search_web_tool],
+    verbose=True,
+    max_iter=5,
+    llm=ChatGroq(api_key=groq_api_key, model='groq/Gemma2-9b-It'),
+    allow_delegation=False,
+)
+
+planner_expert = Agent(
+    role="Travel Planning Expert",
+    goal="Compiles all gathered information to create a travel plan.",
+    backstory="An expert in planning seamless travel itineraries.",
+    tools=[search_web_tool],
+    verbose=True,
+    max_iter=5,
+    llm=ChatGroq(api_key=groq_api_key, model='groq/Gemma2-9b-It'),
+    allow_delegation=False,
+)
